b1ff93683f744a7dd8be8ce2ab18bdca92a7c6b1
Aleksandr Sh <alcovegan@gmail.com>
2018-06-02 15:37:26 +0300

    Resolvers rewritten with universal api resolver

--- resolvers/index.js
+++ resolvers/index.js
@@ -2,48 +2,24 @@ const GraphQLJSON = require('graphql-type-json');
 const apiKey = process.env.TPAPIKEY;
 const api = require('travelpayouts-js')(apiKey);
 
+const universalApiResolver = (name) => (_, args) => api[name](args);
+
 const resolvers = {
     JSON: GraphQLJSON,
     Query: {
-        latest: (_, args) => {
-            return api.latest(args)
-        },
-        monthMatrix: (_, args) => {
-            return api.monthMatrix(args)
-        },
-        weekMatrix: (_, args) => {
-            return api.weekMatrix(args)
-        },
-        nearestPlacesMatrix: (_, args) => {
-            return api.nearestPlacesMatrix(args)
-        },
-        cheap: (_, args) => {
-            return api.cheap(args)
-        },
-        monthly: (_, args) => {
-            return api.monthly(args)
-        },
-        direct: (_, args) => {
-            return api.direct(args)
-        },
-        calendar: (_, args) => {
-            return api.calendar(args)
-        },
-        minPricesCalendar: (_, args) => {
-            return api.minPricesCalendar(args)
-        },
-        directions: (_, args) => {
-            return api.directions(args)
-        },
-        prices: (_, args) => {
-            return api.prices(args)
-        },
-        airline: (_, args) => {
-            return api.airline(args)
-        },
-        city: (_, args) => {
-            return api.city(args)
-        }
+        latest: universalApiResolver('latest'),
+        monthMatrix: universalApiResolver('monthMatrix'),
+        weekMatrix: universalApiResolver('weekMatrix'),
+        nearestPlacesMatrix: universalApiResolver('nearestPlacesMatrix'),
+        cheap: universalApiResolver('cheap'),
+        monthly: universalApiResolver('monthly'),
+        direct: universalApiResolver('direct'),
+        calendar: universalApiResolver('calendar'),
+        minPricesCalendar: universalApiResolver('minPricesCalendar'),
+        directions: universalApiResolver('directions'),
+        prices: universalApiResolver('prices'),
+        airline: universalApiResolver('airline'),
+        city: universalApiResolver('city')
       }
 }
 
